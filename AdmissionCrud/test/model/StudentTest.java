0b8401dc81caa9ca70b0e01a5f04c22291f5875a
Brian Gotti <40281268+bgotti@users.noreply.github.com>
2021-11-03 16:25:47 -0400

    Add files via upload

---
+++ AdmissionCrud/test/model/StudentTest.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Brian
+ */
+public class StudentTest {
+    
+    public StudentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getId method, of class Student.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Student instance = new Student();
+        int expResult = 1;
+        int result = instance.getId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setId method, of class Student.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        int id = 8;
+        Student instance = new Student();
+        instance.setId(id);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFirstName method, of class Student.
+     */
+    @Test
+    public void testGetFirstName() {
+        System.out.println("getFirstName");
+        Student instance = new Student();
+        String expResult = "Kevin";
+        String result = instance.getFirstName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFirstName method, of class Student.
+     */
+    @Test
+    public void testSetFirstName() {
+        System.out.println("setFirstName");
+        String firstName = "Test";
+        Student instance = new Student();
+        instance.setFirstName(firstName);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLastName method, of class Student.
+     */
+    @Test
+    public void testGetLastName() {
+        System.out.println("getLastName");
+        Student instance = new Student();
+        String expResult = "Booker";
+        String result = instance.getLastName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLastName method, of class Student.
+     */
+    @Test
+    public void testSetLastName() {
+        System.out.println("setLastName");
+        String lastName = "Test";
+        Student instance = new Student();
+        instance.setLastName(lastName);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGender method, of class Student.
+     */
+    @Test
+    public void testGetGender() {
+        System.out.println("getGender");
+        Student instance = new Student();
+        String expResult = "M";
+        String result = instance.getGender();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setGender method, of class Student.
+     */
+    @Test
+    public void testSetGender() {
+        System.out.println("setGender");
+        String gender = "M";
+        Student instance = new Student();
+        instance.setGender(gender);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
